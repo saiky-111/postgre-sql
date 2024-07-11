dd1dc7d9d36f4b4233d24d99ef11a4e243c888b9
Saikat Dhibar <saikat111dhibar@gmail.com>
2024-07-11 22:25:19 +0530

    new commit

--- 09_timeStamp_and_Extract_Function.sql
+++ 09_timeStamp_and_Extract_Function.sql
@@ -70,27 +70,47 @@ Output:
 
 --EXTRACT Function
 
--- The
--- EXTRACT() function extracts a part from a given date value.
+-- The EXTRACT() function extracts a part from a given date value.
 -- Syntax:
--- SELECT EXTRACT MONTH FROM date_field ) FROM Table
--- •
--- YEAR
--- •
--- QUARTER
--- •
--- MONTH
--- •
--- WEEK
--- •
--- DAY
--- •
--- HOUR
--- •
--- MINUTE
--- •
--- DOW
--- day of week
--- •
--- DOY
--- day of year
+-- SELECT EXTRACT ( MONTH FROM date_field ) FROM Table
+-- • YEAR
+-- • QUARTER
+-- • MONTH
+-- • WEEK
+-- • DAY
+-- • HOUR
+-- • MINUTE
+-- • DOW -- day of week
+-- • DOY -- day of year
+
+-- (SYNTAX for Extracting Month)
+-- SELECT EXTRACT (MONTH FROM payment_date) AS payment_month, payment_date
+-- FROM payment
+
+-- Written the payment_date at the last, because we wanted to compare the
+-- date with  month.
+
+
+-- (SYNTAX for Extracting Year)
+-- SELECT EXTRACT (YEAR FROM payment_date) AS payment_year, payment_date
+-- FROM payment
+
+
+-- (SYNTAX for Extracting the Day Of Week)
+-- SELECT EXTRACT (DOW FROM payment_date) AS payment_dow, payment_date
+-- FROM payment
+-- 0 will show Sunday, 1 will sho Monday, and so on...
+
+
+-- (SYNTAX for Extracting the Day i.e Date)
+-- SELECT EXTRACT (DATE FROM payment_date) AS payment_day, payment_date
+-- FROM payment
+-- output: 1 for 1st day, 21 for 21st day, 29 for 29th day.
+
+
+-- (SYNTAX for Extracting the Quarter)
+-- SELECT EXTRACT (QUARTER FROM payment_date) AS pay_quarter, payment_date
+-- FROM payment
+-- output: 3 months is 1 quarter.
+--So, 1 : if upto March,
+-- 2: if upto June, and so on..
